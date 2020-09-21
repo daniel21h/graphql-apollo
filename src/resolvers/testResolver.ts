4423933d927a7bb8db8a4d7e3f4b1d50ee80a2fd
daniel21h <hesseldaniel21@gmail.com>
2020-09-20 21:17:32 -0300

    feat: add entities using typeorm

--- src/resolvers/testResolver.ts
+++ src/resolvers/testResolver.ts
@@ -1,6 +1,17 @@
 const testResolver = {
+  Test: {
+    via: (test: any) => {
+      // Operation that returns an object of type media
+      return {
+        name: 'Correio',
+        eta: 365 * 24
+      }
+    }
+  },
   Query: {
-    talk: () => ({ message: 'Hello World' })
+    talk: () => ({
+      message: 'Hello World'
+    })
   }
 }
 
--- src/typedefs/test.ts
+++ src/typedefs/test.ts
@@ -3,6 +3,12 @@ import { gql } from 'apollo-server-express'
 export default gql`
   type Test {
     message: String!
+    via: Media!
+  }
+
+  type Media {
+    name: String!
+    eta: Int!
   }
 
   type Query {
